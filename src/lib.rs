a3e2300befa9e3638f2727d0e811dfb5effd6db9
tayu0110 <tayu.dev@gmail.com>
2026-06-14 03:44:43 +0900

    Add RELAX NG Schemas and namespace constants

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,96 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/// Elements and attributes that describe animation content.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_ANIMATION_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:animation:1.0";
+/// Elements and attributes that describe chart content.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_CHART_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:chart:1.0";
+/// Elements and attributes that describe application specific settings.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_CONFIG_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:config:1.0";
+/// For elements and attributes that describe database specific objects.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_DB_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:database:1.0";
+/// Elements and attributes that describe 3D graphic content.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_DR3D_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0";
+/// Elements and attributes that describe graphic content.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_DRAW_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:drawing:1.0";
+/// Elements and attributes that describe forms and controls.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_FORM_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:form:1.0";
+/// Elements and attribute contained in the package manifest.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_MANIFEST_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:manifest:1.0";
+/// Elements and attributes that describe meta information.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_META_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:meta:1.0";
+/// Elements and attributes that describe data style information.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_NUMBER_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0";
+/// All common pieces of information that are not contained in another, more specific namespace.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_OFFICE_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:office:1.0";
+/// Elements and attributes that describe presentation content.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_PRESENTATION_NAMESPACE: &str =
+    "urn:oasis:names:tc:opendocument:xmlns:presentation:1.0";
+/// Elements and attributes that represent scripts or events.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_SCRIPT_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:script:1.0";
+/// Elements and attributes that may occur within spreadsheets or within table definitions of a text document.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_TABLE_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:table:1.0";
+/// Elements and attributes that may occur within text documents and text parts of other document types.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_TEXT_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:text:1.0";
+/// Elements and attributes that describe the style and inheritance model used by the OpenDocument format.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 1 - XML Namespaces defined by the OpenDocument schema
+pub const ODF_STYLE_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:style:1.0";
+/// RDF node property and node elements for OpenDocument package entities.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 2 - XML Namespaces defined by the OpenDocument metadata manifest ontology
+pub const ODF_ODF_NAMESPACE: &str = "http://docs.oasis-open.org/ns/office/1.2/meta/odf#";
+/// OWL classes and properties contained in metadata manifest files.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 5 - XML Namespaces defined by the OpenDocument metadata manifest ontology
+pub const ODF_PKG_NAMESPACE: &str = "http://docs.oasis-open.org/ns/office/1.2/meta/pkg#";
+/// Namespace for formulas defined by part 2 of this specification that occur in attribute values.
+///
+/// # Reference
+/// - [ISO/IEC 26300-1:2015](https://www.iso.org/standard/66363.html) 1.5 Namespaces Table 6 - Other XML Namespaces defined by this specification
+pub const ODF_OF_NAMESPACE: &str = "urn:oasis:names:tc:opendocument:xmlns:of:1.2";
